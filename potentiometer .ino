f26e5541c6f9e9ce3e422532ec88398f9d6e2a31
Kwame Donkor <donkorkwame994@gmail.com>
2025-07-16 14:53:19 +0100

    Update potentiometer .ino

--- potentiometer .ino
+++ potentiometer .ino
@@ -1,19 +1,17 @@
 const int potPin = 25;
 const int ledPin = 14;
 
-
-void
-setup() {
-pinMode(ledPin, OUTPUT)  
-Serial.begin(115200);
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
 }
 
-void
-loop() {
-int brightness = map(potValue, 0, 4095, 0, 1023);
-int potValue = analogRead(potPin);
+void loop() {
+  int potValue = analogRead(potPin);
+  int brightness = map(potValue, 0, 4095, 0, 255);
+  analogWrite(ledPin, brightness);
 
-Serial.print("Potentiometer Value: ");
-Serial.println(potValue);
-delay(500);
+  Serial.print("Potentiometer Value: ");
+  Serial.println(potValue);
+  delay(500);
 }
